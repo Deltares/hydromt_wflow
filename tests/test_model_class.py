1f6e1987990ab62ab6453842d71297caabd9b816
DirkEilander <dirk.eilander@gmail.com>
2021-04-28 18:26:36 +0200

    plugin tests

--- tests/test_model_class.py
+++ tests/test_model_class.py
@@ -4,9 +4,12 @@ import pytest
 from os.path import join, dirname, abspath
 import numpy as np
 import pdb
+from click.testing import CliRunner
+
 import hydromt
 from hydromt.models import MODELS
 from hydromt.cli.cli_utils import parse_config
+from hydromt.cli.main import main as hydromt_cli
 
 TESTDATADIR = join(dirname(abspath(__file__)), "data")
 EXAMPLEDIR = join(dirname(abspath(__file__)), "..", "examples")
@@ -14,9 +17,8 @@ EXAMPLEDIR = join(dirname(abspath(__file__)), "..", "examples")
 
 def test_model_class():
     # read model in examples folder
-    root = join(EXAMPLEDIR, "wflow_piave_subbasin")
-    model = "wflow"
-    mod = MODELS.get(model)(root=root, mode="r")
+    root = join(EXAMPLEDIR, "plugin_test")
+    mod = MODELS.get("plugin")(root=root, mode="r")
     mod.read()
     # run test_model_api() method
     non_compliant_list = mod.test_model_api()
@@ -27,32 +29,26 @@ def test_model_class():
 def test_model_build(tmpdir):
     # test build method
     # compare results with model from examples folder
-    model = "wflow"
+    model = "plugin"
     root = str(tmpdir.join(model))
-    mod1 = MODELS.get(model)(root=root, mode="w")
-    # Build method options
-    region = {
-        "subbasin": [12.2051, 45.8331],
-        "strord": 4,
-        "bbox": [11.70, 45.35, 12.95, 46.70],
-    }
-    res = 0.01666667
-    config = join(TESTDATADIR, "wflow_piave_build_subbasin.ini")
-    opt = parse_config(config)
+    config = join(EXAMPLEDIR, "model_build.ini")
+    region = "{'bbox': [11.70, 45.35, 12.95, 46.70]}"
     # Build model
-    mod1.build(region=region, res=res, opt=opt)
-    # Check if model is api compliant
-    non_compliant_list = mod1.test_model_api()
-    assert len(non_compliant_list) == 0
+    r = CliRunner().invoke(
+        hydromt_cli, ["build", model, root, region, "-i", config, "-vv"]
+    )
+    assert r.exit_code == 0
 
     # Compare with model from examples folder
-    root = join(EXAMPLEDIR, "wflow_piave_subbasin")
-    mod0 = MODELS.get(model)(root=root, mode="r")
+    root0 = join(EXAMPLEDIR, "plugin_test")
+    mod0 = MODELS.get(model)(root=root0, mode="r")
     mod0.read()
+    mod1 = MODELS.get(model)(root=root, mode="r")
+    mod1.read()
     # check maps
     invalid_maps = []
     if len(mod0._staticmaps) > 0:
-        maps = mod0.staticmaps.rio.vars
+        maps = mod0.staticmaps.raster.vars
         assert np.all(mod0.crs == mod1.crs), f"map crs {name}"
         for name in maps:
             map0 = mod0.staticmaps[name].fillna(0)
