7a7f7641a4962949dc4666e5c35b8e90805cd9f9
JoostBuitink <44062204+JoostBuitink@users.noreply.github.com>
2022-11-16 12:15:41 +0100

    fix typo in docstring

--- hydromt_wflow/wflow.py
+++ hydromt_wflow/wflow.py
@@ -586,17 +586,17 @@ class WflowModel(Model):
     ):
         """
         This component derives several wflow maps are derived based on landuse-
-        landcover (LULC) data. 
-        
+        landcover (LULC) data.
+
         Currently, ``lulc_fn`` can be set to the "vito", "globcover"
         or "corine", fo which lookup tables are constructed to convert lulc classses to
         model parameters based on literature. The data is remapped at its original
         resolution and then resampled to the model resolution using the average
         value, unless noted differently.
-        
+
         Adds model layers:
 
-        * **landuse** map: Landuse class [-]     
+        * **landuse** map: Landuse class [-]
         * **Kext** map: Extinction coefficient in the canopy gap fraction equation [-]
         * **Sl** map: Specific leaf storage [mm]
         * **Swood** map: Fraction of wood in the vegetation/plant [-]
@@ -1798,7 +1798,7 @@ class WflowModel(Model):
             For more options, see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
         chunksize: int, optional
             Chunksize on time dimension when saving to disk. By default 1.
-        decimals, int, optional
+        decimals: int, optional
             Round the ouput data to the given number of decimals.
         time_units: str, optional
             Common time units when writting several netcdf forcing files. By default "days since 1900-01-01T00:00:00".
