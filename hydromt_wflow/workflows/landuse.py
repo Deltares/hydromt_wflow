976326827912bcb073c391db99fe1ec8436addff
bouaziz <laurene.bouaziz@deltares.nl>
2023-03-23 09:37:27 +0100

    remove changes crs

--- hydromt_wflow/workflows/landuse.py
+++ hydromt_wflow/workflows/landuse.py
@@ -55,9 +55,7 @@ def landuse(da, ds_like, fn_map, logger=logger, params=None):
     def reclass(x):
         return np.vectorize(d.get)(x, nodata)
     
-    da_crs = da.raster.crs
     da = da.raster.interpolate_na(method="nearest")
-    da.raster.set_crs(da_crs)
     # apply for each parameter
     for param in params:
         method = RESAMPLING.get(param, "average")
