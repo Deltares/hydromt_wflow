05ed01de50a6715ac6a006ffa3f50e7529ea0d7c
Laurene Bouaziz <laurene.bouaziz@deltares.nl>
2023-06-13 17:32:59 +0200

    expand test

--- tests/test_model_methods.py
+++ tests/test_model_methods.py
@@ -50,6 +50,12 @@ def test_setup_rootzoneclim():
     ds["pet"] = ds["pet"] + test_data["pet"].to_xarray()
     ds["precip"] = ds["precip"] + test_data["precip"].to_xarray()
 
+    ds_cc_hist = ds.copy()
+    ds_cc_hist["pet"] = ds["pet"] * 1.05
+
+    ds_cc_fut = ds.copy()
+    ds_cc_fut["pet"] = ds["pet"] * 1.3
+
     #make dummy dataset with dummy Q data
     indices = [1,2,3]
     ds_run = xr.Dataset(
@@ -70,14 +76,49 @@ def test_setup_rootzoneclim():
     mod.setup_rootzoneclim(
                         run_fn=ds_run, 
                         forcing_obs_fn=ds, 
+                        forcing_cc_hist_fn=ds_cc_hist,
+                        forcing_cc_fut_fn=ds_cc_fut,
                         start_hydro_year = "Oct",
                         start_field_capacity = "Apr",                       
                         time_tuple = ("2005-01-01", "2015-12-31"), 
+                        time_tuple_fut = ("2005-01-01", "2017-12-31"), 
                         missing_days_threshold=330,
-                        return_period = [2,5,10,15,20]
+                        return_period = [2,5,10,15,20],
+                        update_toml_rootingdepth = "RootingDepth_obs_2",
                         )
 
     assert "RootingDepth_obs_20" in mod.staticmaps
+    assert "RootingDepth_cc_hist_20" in mod.staticmaps
+    assert "RootingDepth_cc_fut_20" in mod.staticmaps
+
     assert "rootzone_storage_obs_15" in mod.staticmaps
+    assert "rootzone_storage_cc_hist_15" in mod.staticmaps
+    assert "rootzone_storage_cc_fut_15" in mod.staticmaps
+
+    assert mod.get_config("input.vertical.rootingdepth") == "RootingDepth_obs_2"
+
     assert mod.staticgeoms["rootzone_storage"].loc[1]["rootzone_storage_obs_2"] == pytest.approx(72.32875652734612, abs=0.5)
+    assert mod.staticgeoms["rootzone_storage"].loc[1]["rootzone_storage_cc_hist_2"] == pytest.approx(70.26993058321949, abs=0.5)
+    assert mod.staticgeoms["rootzone_storage"].loc[1]["rootzone_storage_cc_fut_2"] == pytest.approx(80.89081789601374, abs=0.5)
+
+    #change settings for LAI and correct_cc_deficit
+    mod.setup_rootzoneclim(
+                    run_fn=ds_run, 
+                    forcing_obs_fn=ds, 
+                    forcing_cc_hist_fn=ds_cc_hist,
+                    forcing_cc_fut_fn=ds_cc_fut,
+                    LAI = True,
+                    start_hydro_year = "Oct",
+                    start_field_capacity = "Apr",                       
+                    time_tuple = ("2005-01-01", "2015-12-31"), 
+                    time_tuple_fut = ("2005-01-01", "2017-12-31"), 
+                    correct_cc_deficit = True,
+                    missing_days_threshold=330,
+                    return_period = [2,5,10,15,20],
+                    update_toml_rootingdepth = "RootingDepth_obs_2",
+                    )
+    
+    assert mod.staticgeoms["rootzone_storage"].loc[1]["rootzone_storage_obs_2"] == pytest.approx(82.85684577620462, abs=0.5)
+    assert mod.staticgeoms["rootzone_storage"].loc[1]["rootzone_storage_cc_hist_2"] == pytest.approx(82.44039441508069, abs=0.5)
+    assert mod.staticgeoms["rootzone_storage"].loc[1]["rootzone_storage_cc_fut_2"] == pytest.approx(104.96931418911882, abs=0.5)
 
