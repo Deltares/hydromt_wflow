7e74ede6280ddb6479dc9034a2111dce3b31ef96
Brendan <brencodeert@outlook.com>
2023-10-20 14:00:49 +0200

    Small rewrite

--- hydromt_wflow/__init__.py
+++ hydromt_wflow/__init__.py
@@ -5,7 +5,7 @@ from os.path import abspath, dirname, join
 __version__ = "0.3.1.dev0"
 
 try:
-    import pcraster as pcrs
+    import pcraster as pcr
 
     HAS_PCRASTER = True
 except ImportError:
