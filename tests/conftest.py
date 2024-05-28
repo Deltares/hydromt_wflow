fc83571742f5dc251cd87b4f103824d205ac7476
JoostBuitink <44062204+JoostBuitink@users.noreply.github.com>
2024-05-28 08:52:36 +0200

    Add support for `alpha_h1` parameter and the `GLCNMO` dataset (#272)
    * add support for glcnmo and alpha_h1
    
    * Update wflow.py
    
    * fix wflow_sediment lulc_vars
    
    * update (windows) test data
    
    * update linux test data
    
    * update changelog
    
    * fix review comments
    
    * fix linux test
    
    * improve resampling; update test data
    
    * update linux test data
    
    * Update hydromt_wflow/workflows/landuse.py
    
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,12 @@ EXAMPLEDIR = join(dirname(abspath(__file__)), "..", "examples", SUBDIR)
 def example_wflow_model():
     logger = logging.getLogger(__name__)
     root = join(EXAMPLEDIR, "wflow_piave_subbasin")
-    mod = WflowModel(root=root, mode="r", data_libs="artifact_data", logger=logger)
+    mod = WflowModel(
+        root=root,
+        mode="r",
+        data_libs=["artifact_data"],
+        logger=logger,
+    )
     return mod
 
 
@@ -34,7 +39,10 @@ def example_sediment_model():
     logger = logging.getLogger(__name__)
     root = join(EXAMPLEDIR, "wflow_sediment_piave_subbasin")
     mod = WflowSedimentModel(
-        root=root, mode="r", data_libs="artifact_data", logger=logger
+        root=root,
+        mode="r",
+        data_libs=["artifact_data"],
+        logger=logger,
     )
     return mod
 
@@ -76,7 +84,11 @@ def example_wflow_results():
 @pytest.fixture()
 def clipped_wflow_model():
     root = join(EXAMPLEDIR, "wflow_piave_clip")
-    mod = WflowModel(root=root, mode="r", data_libs="artifact_data")
+    mod = WflowModel(
+        root=root,
+        mode="r",
+        data_libs=["artifact_data"],
+    )
     return mod
 
 
