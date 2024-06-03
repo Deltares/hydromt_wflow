098741ae9db2571316e23a1f6103903ca8ae9101
Brendan <brencodeert@outlook.com>
2024-06-03 17:18:13 +0200

    Groundwater setup

---
+++ hydromt_wflow/workflows/groundwater.py
@@ -0,0 +1,22 @@
+"""Groundwater workflows."""
+
+import logging
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+__all__ = ["acquifer_thickness", "constant_boundary", "conductivity"]
+
+
+def acquifer_thickness():
+    pass
+
+
+def constant_boundary():
+    pass
+
+
+def conductivity():
+    pass
