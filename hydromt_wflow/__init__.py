048f96b05bf4ffe3334b40605fb182810535bdd1
dalmijn <brencodeert@outlook.com>
2025-06-04 11:13:31 +0200

    Setuptools backend

--- hydromt_wflow/__init__.py
+++ hydromt_wflow/__init__.py
@@ -1,7 +1,6 @@
 """HydroMT plugin for wflow models."""
 
-__version__ = "1.0.0.dev"
-
+from .version import __version__
 from .wflow import WflowModel
 from .wflow_sediment import WflowSedimentModel
 
