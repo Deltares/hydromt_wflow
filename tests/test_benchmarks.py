1b35937159218ed862e8e15ea5681e91c3921d5a
Sam Vente <savente93@proton.me>
2025-06-12 16:52:51 +0200

    feat: add simple benchmark code and tasks

---
+++ tests/test_benchmarks.py
@@ -0,0 +1,28 @@
+"""Test plugin model class against hydromt.models.model_api."""
+
+from uuid import uuid4
+
+from hydromt_wflow.wflow import WflowModel
+
+
+def inner_benchmark_loop(tmpdir, wflow_ini):
+    # don't interfere with output of other rounds
+    root = tmpdir.join(str(uuid4()))
+    mod1 = WflowModel(root=root, mode="w", data_libs="artifact_data")
+    # Build method options
+    region = {
+        "subbasin": [12.2051, 45.8331],
+        "strord": 4,
+        "bounds": [11.70, 45.35, 12.95, 46.70],
+    }
+    # get ini file
+    opt = wflow_ini
+
+    # Build model
+    mod1.build(region=region, opt=opt)
+
+
+def test_outer_benchmark(tmpdir, wflow_ini, benchmark):
+    benchmark.pedantic(
+        inner_benchmark_loop, (tmpdir, wflow_ini), iterations=1, rounds=10
+    )
