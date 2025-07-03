b84f70c4df73f36ed41845a7936be5eace28be4c
LuukBlom <153174893+LuukBlom@users.noreply.github.com>
2025-07-03 10:42:26 +0200

    feat: add geoms component (#432)
    * Update ruff settings in pyproject
    
    * implement WflowGeomsComponent, move all relevant code from setup_basemaps into component
    
    * updated set_geoms() -> geoms.set() in wflow_sedimentpy and wflow.py
    
    * update geoms access and remove comment
    
    * Add test for `WflowGeomsComponent.parse_region`
    
    * fix incorrect type hints
    
    * formatting
    
    * update lockfile and test
    
    * added test for WflowGeomsComponent get method
    
    * added test for WflowGeomsComponent pop method
    
    * added test for testing raised errors
    
    * added furter tests on different region kinds
    
    * removed grid component
    
    * refactor scale_ratio in parse_region
    
    * renamed variables for clarity
    
    * separate big error test into smaller cases
    
    * escape parentheses in test
    
    * implement read and write functions for WflowGeomsComponent
    
    * moved parse_region into workflows.basemaps and updated tests
    
    * resolve test errors
    
    * Restructure and re-use fixtures
    
    * revert accidental name changes
    
    * Update hydromt_wflow/components/geoms.py
    
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>
    
    * Update hydromt_wflow/wflow.py
    
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>
    
    * Update hydromt_wflow/wflow.py
    
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>
    
    * part of review comments
    
    * small review comments
    
    * implement review comments and update tests
    
    * docstring updates and test cleanup
    
    * Update hydromt_wflow/components/geoms.py
    
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>
    
    * Update hydromt_wflow/components/geoms.py
    
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>
    
    * review comments: removed example in docstring. default filename is now staticgeoms. use read() and write() from core GeomsComponent. Updated tests
    
    * fix if statement
    
    * update documentation
    
    * review comments
    
    ---------
    
    Co-authored-by: Tjalling-dejong <tjalling.dejong@deltares.nl>
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>

--- hydromt_wflow/components/__init__.py
+++ hydromt_wflow/components/__init__.py
@@ -1,11 +1,13 @@
 """Wflow model components submodule."""
 
-from .config import WflowConfigComponent
-from .states import WflowStatesComponent
-from .staticmaps import StaticmapsComponent
+from hydromt_wflow.components.config import WflowConfigComponent
+from hydromt_wflow.components.geoms import WflowGeomsComponent
+from hydromt_wflow.components.states import WflowStatesComponent
+from hydromt_wflow.components.staticmaps import StaticmapsComponent
 
 __all__ = [
     "StaticmapsComponent",
     "WflowConfigComponent",
     "WflowStatesComponent",
+    "WflowGeomsComponent",
 ]
