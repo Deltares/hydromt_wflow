197eda9818295f54aca8b2ac691abf00fe7f5904
LuukBlom <153174893+LuukBlom@users.noreply.github.com>
2025-07-10 09:18:32 +0200

    fix: conditional reprojection if shapes of the mask & data do not align (#497)
    * added a conditional reprojection to the `mask_raster_from_layer` util function if the shapes of the mask & data do not align
    
    * brendans clean improvement

--- hydromt_wflow/utils.py
+++ hydromt_wflow/utils.py
@@ -13,7 +13,7 @@ from hydromt.vector import GeoDataArray
 from hydromt.workflows.grid import grid_from_constant
 from tomlkit.items import Key
 
-from .naming import (
+from hydromt_wflow.naming import (
     WFLOW_NAMES,
     WFLOW_SEDIMENT_NAMES,
     WFLOW_SEDIMENT_STATES_NAMES,
@@ -526,6 +526,10 @@ def mask_raster_from_layer(
     -------
         xr.Dataset, xr.DataArray: The grid with all of the data variables masked.
     """
+    # Reproject data to match mask's grid if shapes differ
+    if data.sizes != mask.sizes:
+        data = data.raster.reproject_like(mask)
+
     mask = mask != mask.raster.nodata
     # Need to duplicate or else data should have a name ie we duplicate functionality
     # of GridModel.set_grid
