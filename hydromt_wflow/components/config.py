ee0d6627898a42af64b7367bdcd61a4d7cde9693
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-07-14 10:30:30 +0200

    changed docstring

--- hydromt_wflow/components/config.py
+++ hydromt_wflow/components/config.py
@@ -18,21 +18,10 @@ logger = logging.getLogger(f"hydromt.{__name__}")
 
 
 class WflowConfigComponent(ConfigComponent):
-    """A component to manage configuration files for model simulations/settings.
+    """Manage the wflow TOML configuration file for model simulations/settings.
 
-    Parameters
-    ----------
-    model: Model
-        HydroMT model instance
-    filename: str
-        A path relative to the root where the configuration file will
-        be read and written if user does not provide a path themselves.
-        By default 'config.yml'
-    default_template_filename: Optional[Path]
-        A path to a template file that will be used as default in the ``create``
-        method to initialize the configuration file if the user does not provide
-        their own template file. This can be used by model plugins to provide a
-        default configuration template. By default None.
+    ``WflowConfigComponent`` data is stored in a dictionary. The component
+    is used to prepare and update model simulations/settings of the wflow model.
     """
 
     def __init__(
@@ -42,6 +31,22 @@ class WflowConfigComponent(ConfigComponent):
         filename: str = "wflow_sbm.toml",
         default_template_filename: str | None = None,
     ):
+        """Manage configuration files for model simulations/settings.
+
+        Parameters
+        ----------
+        model: Model
+            HydroMT model instance
+        filename: str
+            A path relative to the root where the configuration file will
+            be read and written if user does not provide a path themselves.
+            By default 'config.yml'
+        default_template_filename: Optional[Path]
+            A path to a template file that will be used as default in the ``create``
+            method to initialize the configuration file if the user does not provide
+            their own template file. This can be used by model plugins to provide a
+            default configuration template. By default None.
+        """
         super().__init__(
             model,
             filename=filename,
