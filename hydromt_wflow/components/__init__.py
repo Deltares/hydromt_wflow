bb4f415397e4a23063d467733734687100d76c8d
Luuk Blom <luuk.blom@deltares.nl>
2025-08-12 15:16:07 +0200

    fix merge conflict mistake

--- hydromt_wflow/components/__init__.py
+++ hydromt_wflow/components/__init__.py
@@ -1,6 +1,7 @@
 """Wflow model components submodule."""
 
 from hydromt_wflow.components.config import WflowConfigComponent
+from hydromt_wflow.components.forcing import WflowForcingComponent
 from hydromt_wflow.components.geoms import WflowGeomsComponent
 from hydromt_wflow.components.states import WflowStatesComponent
 from hydromt_wflow.components.staticmaps import WflowStaticmapsComponent
