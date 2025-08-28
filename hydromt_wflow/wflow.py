862d9a7cf49012489c79986842fa826aa98c7325
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-08-28 17:04:12 +0200

    added driver kwarg

--- hydromt_wflow/wflow.py
+++ hydromt_wflow/wflow.py
@@ -1690,6 +1690,7 @@ skipping adding gauge specific outputs to the toml."
             "river_water__volume_flow_rate",
             "atmosphere_water__precipitation_volume_flux",
         ],
+        driver: str | None = None,
         **kwargs,
     ):
         """Set a gauge map based on ``gauges_fn`` data.
@@ -1811,6 +1812,9 @@ gauge locations [-] (if derive_subcatch)
                     kwargs["metadata"].update(crs=code)
                 else:
                     kwargs.update(metadata={"crs": code})
+                if not driver:
+                    driver = "geodataframe_table"
+                kwargs.update({"driver": {"name": driver}})
 
             gdf_gauges = self.data_catalog.get_geodataframe(
                 gauges_fn,
