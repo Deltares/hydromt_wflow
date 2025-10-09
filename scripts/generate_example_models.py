3f7aef69ba69fca4f8c15c84aeced79a7b1571d2
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-10-09 13:08:37 +0200

    updated linux64 example model

--- scripts/generate_example_models.py
+++ scripts/generate_example_models.py
@@ -5,6 +5,15 @@ from hydromt.io.readers import read_workflow_yaml
 
 from hydromt_wflow import WflowSbmModel
 
+
+def remove_files(files):
+    """Remove files if they exist."""
+    for file in files:
+        p = Path(file)
+        if p.exists():
+            p.unlink()
+
+
 if __name__ == "__main__":
     repo_root = Path(__file__).parent.parent
     if os.name == "posix":
@@ -12,15 +21,15 @@ if __name__ == "__main__":
     else:
         model_root = repo_root / "examples" / "wflow_piave_subbasin"
 
-    # Remove staticmaps file if exists
-    staticmaps_file = model_root / "staticmaps.nc"
-    if staticmaps_file.exists():
-        staticmaps_file.unlink()
+    # Remove staticmaps and inmaps.nc files if exists
+    files = [model_root / "staticmaps.nc", model_root / "inmaps.nc"]
+    remove_files(files)
 
     param_path = repo_root / "hydromt_wflow" / "data" / "parameters_data.yml"
 
     model = WflowSbmModel(
         root=model_root.as_posix(),
+        config_filename=model_root / "wflow_sbm.toml",
         mode="w",
         data_libs=["artifact_data", param_path.as_posix()],
     )
@@ -35,7 +44,4 @@ if __name__ == "__main__":
         "reservoir_accuracy.csv",
         "reservoir_timeseries_None.csv",  # Probably bug
     ]
-    for file in files:
-        p = model_root / file
-        if p.exists():
-            p.unlink()
+    remove_files([model_root / f for f in files])
