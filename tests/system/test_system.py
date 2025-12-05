733df1d4aba4ab7fac3cc99b4489b70415ee295a
Luuk Blom <luuk.blom@deltares.nl>
2025-12-05 10:34:48 +0100

    add pytest marks and pixi tasks for system and integration tests.
    `pixi run unit-test` -- this runs 118 tests (in 30 seconds), runs all except for system and integration.
    `pixi run integration-test` -- runs 47 tests
    `pixi run system-test` -- runs 1 system test
    `pixi run test` -- runs all except for system.

--- tests/system/test_system.py
+++ tests/system/test_system.py
@@ -6,40 +6,40 @@ It assumes that julia and hydromt are installed and available in the system PATH
 import logging
 import shutil
 import subprocess
-import tempfile
 from pathlib import Path
 
-from hydromt import initialize_logging
+import pytest
 
 logger = logging.getLogger(__name__)
 
 EXAMPLE_DIR = Path(__file__).parents[2] / "examples"
 INTEGRATION_TESTS_DIR = Path(__file__).parent
+pytestmark = pytest.mark.system
 
 
-def main():
-    """Run the integration tests using hydromt_wflow and Julia.
+@pytest.mark.skipif(shutil.which("julia") is None, reason="Julia not installed.")
+def test_sbm_and_sediment_build_and_run_julia(tmp_path: Path):
+    """Run the system test using hydromt_wflow and Julia.
 
-    This function generates a water demand model, then runs it.
-    Next, it generates a sediment model based on the water demand model
-    and runs that as well.
+    This test:
+        1. generates a water demand model
+        2. runs it.
+        3. generates a sediment model based on the water demand model
+        4. runs that as well.
     """
-    initialize_logging()
-
     logger.info("Building sbm model with water demand data...")
-    sbm_config = generate_water_demand_model()
+    sbm_config = generate_water_demand_model(tmp_path / "wflow_sbm")
     logger.info("Running sbm model...")
-    run_wflow_julia(sbm_config)
+    assert run_wflow_julia(sbm_config)
 
     logger.info("Building sediment model from sbm model...")
     sediment_config = generate_sediment_model(sbm_config.parent)
     logger.info("Running sediment model...")
-    run_wflow_julia(sediment_config)
+    assert run_wflow_julia(sediment_config)
 
 
-def generate_water_demand_model() -> Path:
+def generate_water_demand_model(root: Path) -> Path:
     """Generate a wflow_sbm model with water demand data."""
-    root = Path(tempfile.gettempdir(), "wflow_sbm").resolve()
     build_workflow_yml: Path = INTEGRATION_TESTS_DIR / "wflow_build_sbm.yml"
     demand_data_catalog: Path = EXAMPLE_DIR / "data" / "demand" / "data_catalog.yml"
 
@@ -58,7 +58,7 @@ def generate_water_demand_model() -> Path:
     ]
 
     shutil.rmtree(root, ignore_errors=True)
-    run_command(command)
+    assert run_command(command)
     config_file = root / "wflow_sbm.toml"
     assert config_file.exists(), f"Config file {config_file.as_posix()} does not exist."
     return config_file
@@ -86,7 +86,7 @@ def generate_sediment_model(sbm_root: Path) -> Path:
         "--fo",
         "-v",
     ]
-    run_command(command)
+    assert run_command(command)
 
     logger.info(f"Generated sediment model at {sbm_root.as_posix()}")
     config_file = sbm_root / "wflow_sediment.toml"
@@ -114,7 +114,7 @@ def run_command(command: list[str]) -> bool:
     return success
 
 
-def run_wflow_julia(toml_path: Path):
+def run_wflow_julia(toml_path: Path) -> bool:
     """Run a wflow model using Julia and a TOML configuration file."""
     command = [
         "julia",
@@ -125,8 +125,4 @@ def run_wflow_julia(toml_path: Path):
         Wflow.run(toml_path)
         """,
     ]
-    run_command(command)
-
-
-if __name__ == "__main__":
-    main()
+    return run_command(command)
