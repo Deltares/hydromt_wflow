a0e90ace9f9a37caf5c5a16a91b2810a27588bc0
Luuk Blom <luuk.blom@deltares.nl>
2025-12-18 14:17:10 +0100

    update build command

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -9,17 +9,9 @@ def inner_benchmark_loop(tmpdir, wflow_ini):
     # don't interfere with output of other rounds
     root = tmpdir.join(str(uuid4()))
     mod1 = WflowSbmModel(root=root, mode="w", data_libs="artifact_data")
-    # Build method options
-    region = {
-        "subbasin": [12.2051, 45.8331],
-        "strord": 4,
-        "bounds": [11.70, 45.35, 12.95, 46.70],
-    }
-    # get ini file
-    opt = wflow_ini
 
     # Build model
-    mod1.build(region=region, opt=opt)
+    mod1.build(steps=wflow_ini)
 
 
 def test_outer_benchmark(tmpdir, wflow_ini, benchmark):
