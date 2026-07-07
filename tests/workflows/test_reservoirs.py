2d5f46bec3471736a69f7fd691488064d8036f06
Luuk Blom <luuk.blom@deltares.nl>
2026-07-07 13:16:45 +0200

    Squash PR #777: fix-reservoirs-outside-rivers

--- tests/workflows/test_reservoirs.py
+++ tests/workflows/test_reservoirs.py
@@ -1,7 +1,7 @@
 import numpy as np
 import xarray as xr
 
-from hydromt_wflow.workflows.reservoirs import set_rating_curve_layer_data_type
+from hydromt_wflow.workflows import reservoirs
 
 
 def test_set_rating_curve_layer_data_type():
@@ -18,11 +18,48 @@ def test_set_rating_curve_layer_data_type():
     ds = xr.Dataset({"reservoir_rating_curve": da})
     ds2 = xr.Dataset({"reservoir_rating_curve": da2})
 
-    ds = set_rating_curve_layer_data_type(ds)
-    ds2 = set_rating_curve_layer_data_type(ds2)
+    ds = reservoirs.set_rating_curve_layer_data_type(ds)
+    ds2 = reservoirs.set_rating_curve_layer_data_type(ds2)
 
     assert ds["reservoir_rating_curve"].dtype == np.int32
     assert ds2["reservoir_rating_curve"].dtype == np.int32
     assert ds["reservoir_rating_curve"].raster.nodata == -999
     assert ds2["reservoir_rating_curve"].attrs["_FillValue"] == -999
     assert ds["reservoir_rating_curve"][0, 0].values == -999
+
+
+def test_exclude_reservoirs_outside_rivers():
+    """Test that reservoirs outside the river network are excluded."""
+    # Create a river mask river, cells = 1, non-river = 0
+    river_mask = xr.DataArray(
+        [
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+    )
+
+    # Create reservoir IDs, overlaps river = 10, no overlap = 20
+    reservoir_ids = xr.DataArray(
+        [
+            [-999, 10, 10, -999, -999],
+            [-999, 10, -999, -999, -999],
+            [-999, -999, -999, -999, -999],
+            [-999, -999, -999, 20, 20],
+            [-999, -999, -999, 20, 20],
+        ]
+    )
+
+    exclude_reservoirs = reservoirs.exclude_reservoirs_outside_rivers(
+        river_mask, reservoir_ids, exclude_outside_reservoirs=True
+    )
+    include_reservoirs = reservoirs.exclude_reservoirs_outside_rivers(
+        river_mask, reservoir_ids
+    )
+
+    assert 10 in exclude_reservoirs.values
+    assert 20 not in exclude_reservoirs.values
+    assert 10 in include_reservoirs.values
+    assert 20 in include_reservoirs.values
